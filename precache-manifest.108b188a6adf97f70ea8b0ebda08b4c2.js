58d696c68f116547158eb37c5f54514ad2315e6f
jyouj <youj.whenever.prime3@gmail.com>
2020-04-14 18:16:53 +0900

    Updates

---
+++ precache-manifest.108b188a6adf97f70ea8b0ebda08b4c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642b8d4093868461c7667b1f50cea6fd",
+    "url": "/livre/index.html"
+  },
+  {
+    "revision": "4982b8c9a5aa536d1b1f",
+    "url": "/livre/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "5ee71f253c4496c70d9c",
+    "url": "/livre/static/css/main.6d2f07c6.chunk.css"
+  },
+  {
+    "revision": "4982b8c9a5aa536d1b1f",
+    "url": "/livre/static/js/2.1cb04ad2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/livre/static/js/2.1cb04ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee71f253c4496c70d9c",
+    "url": "/livre/static/js/main.7a67c49e.chunk.js"
+  },
+  {
+    "revision": "d210a446d8275a3d8994",
+    "url": "/livre/static/js/runtime-main.c46707be.js"
+  }
+]);
